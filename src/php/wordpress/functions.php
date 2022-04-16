523a7e8eef3c073245116eef81c59a2fda5057ca
cuongtobi <cuongvuong299@gmail.com>
2022-04-16 14:34:04 +0700

    :pencil: [PHP] Wordpress functions

---
+++ src/php/wordpress/functions.php
@@ -0,0 +1,6 @@
+<?php
+remove_action('wp_head', 'rsd_link'); //removes EditURI/RSD (Really Simple Discovery) link.
+remove_action('wp_head', 'wlwmanifest_link'); //removes wlwmanifest (Windows Live Writer) link.
+remove_action('wp_head', 'wp_generator'); //removes meta name generator.
+remove_action('wp_head', 'wp_shortlink_wp_head'); //removes shortlink.
+remove_action('wp_head', 'feed_links', 2 );  //removes automatic feed links.
