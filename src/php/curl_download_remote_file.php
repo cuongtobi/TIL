baefbb80ec8cc56cf19d78d2c3cfbacb7a40c11d
Cuong Vuong <cuongtobi29@gmail.com>
2022-04-18 14:24:25 +0700

    :pencil: [PHP] cUrl download remote file

---
+++ src/php/curl_download_remote_file.php
@@ -0,0 +1,42 @@
+<?php
+function download_remote_file($url, $filename) {
+    $file = 'path/to/' . $filename;
+
+    if (!file_exists($file)) {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        file_put_contents($file, $response);
+    }
+
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename=' . basename($file));
+    header('Content-Transfer-Encoding: binary');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    ob_clean();
+    flush();
+    readfile($file);
+    unlink($file);
+
+    exit;
+}
