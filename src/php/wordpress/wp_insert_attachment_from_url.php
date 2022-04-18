a57950d475c64c3dfe2195de1fd4c4fde658c80b
Cuong Vuong <cuongtobi29@gmail.com>
2022-04-18 14:28:27 +0700

    :pencil: [PHP] Wordpress insert attachment from url

---
+++ src/php/wordpress/wp_insert_attachment_from_url.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Insert an attachment from an URL address.
+ *
+ * @param  String $url
+ * @param  Int    $parent_post_id
+ * @return Int    Attachment ID
+ */
+function crb_insert_attachment_from_url($url, $parent_post_id = null) {
+
+	if( !class_exists( 'WP_Http' ) )
+		include_once( ABSPATH . WPINC . '/class-http.php' );
+
+	$http = new WP_Http();
+	$response = $http->request( $url );
+	if( $response['response']['code'] != 200 ) {
+		return false;
+	}
+
+	$upload = wp_upload_bits( basename($url), null, $response['body'] );
+	if( !empty( $upload['error'] ) ) {
+		return false;
+	}
+
+	$file_path = $upload['file'];
+	$file_name = basename( $file_path );
+	$file_type = wp_check_filetype( $file_name, null );
+	$attachment_title = sanitize_file_name( pathinfo( $file_name, PATHINFO_FILENAME ) );
+	$wp_upload_dir = wp_upload_dir();
+
+	$post_info = array(
+		'guid'           => $wp_upload_dir['url'] . '/' . $file_name,
+		'post_mime_type' => $file_type['type'],
+		'post_title'     => $attachment_title,
+		'post_content'   => '',
+		'post_status'    => 'inherit',
+	);
+
+	// Create the attachment
+	$attach_id = wp_insert_attachment( $post_info, $file_path, $parent_post_id );
+
+	// Include image.php
+	require_once( ABSPATH . 'wp-admin/includes/image.php' );
+
+	// Define attachment metadata
+	$attach_data = wp_generate_attachment_metadata( $attach_id, $file_path );
+
+	// Assign metadata to attachment
+	wp_update_attachment_metadata( $attach_id,  $attach_data );
+
+	return $attach_id;
+
+}
